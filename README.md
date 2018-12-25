45a9a104453643f2b06717f39d2911cb53aecaaa
Tavinen <46123926+Tavinen@users.noreply.github.com>
2018-12-25 18:55:09 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TavinenBotv2
+# Discord.py MusicBot
+
+A simple musicbot written in Discord.py that utilizes YoutubeDL and ffmpeg to stream audio. Use the `help` command to get a list of commands!
---
+++ src/config.py
@@ -0,0 +1,27 @@
+import toml, logging, os
+
+EXAMPLE_CONFIG = """\"NTI3MDA3NDE2MTI3MDYyMDM3.DwOreg.cR6Grg2nYszPsDj09f_vcc200Oc\"=\"\" # the bot's token
+\"T\"=\"!\" # prefix used to denote commands
+
+[music]
+# Options for the music commands
+"max_volume"=250 # Max audio volume. Set to -1 for unlimited.
+"vote_skip"=true # whether vote-skipping is enabled
+"vote_skip_ratio"=0.5 # the minimum ratio of votes needed to skip a song
+[tips]
+"github_url"="https://github.com/joek13/py-music-bot"
+"""
+
+
+def load_config(path="./config.toml"):
+    """Loads the config from `path`"""
+    if os.path.exists(path) and os.path.isfile(path):
+        config = toml.load(path)
+        return config
+    else:
+        with open(path, "w") as config:
+            config.write(EXAMPLE_CONFIG)
+            logging.warn(
+                f"No config file found. Creating a default config file at {path}"
+            )
+        return load_config(path=path)
